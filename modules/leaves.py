b3559a95e40f10c13b4faa2a93976f9f6805fe7a
Mocha <mocha2007andcoco@gmail.com>
2019-01-03 22:40:50 -0500

    leaf decay

--- modules/leaves.py
+++ modules/leaves.py
@@ -4,19 +4,29 @@ from common import get_block_by_name, von_neumann_neighborhood
 
 def main(**kwargs) -> list:
 	"""
-	Regrows leaves on wood under certain conditions
+	Regrows or decays leaves on wood under certain conditions
 	"""
 	blocks = kwargs['blocks']
 	world = kwargs['world']
 	leaves = get_block_by_name(blocks, 'leaves')
 	wood = get_block_by_name(blocks, 'wood')
+	# check for decay
+	for y in range(len(world)):
+		for x in range(len(world[0])):
+			block = world[y][x]
+			if block == leaves and random() < 1/10:
+				vnns = set(von_neumann_neighborhood((x, y), world))
+				if wood not in vnns:
+					world[y][x] = None
+	# check for regrowth
 	for y in range(len(world)):
 		wood_check_area = (world[y-1] if y else []) + world[y] + (world[y+1] if y+1 < len(world) else [])
 		if wood not in wood_check_area:
 			continue
 		for x in range(len(world[0])):
 			block = world[y][x]
-			vnns = set(von_neumann_neighborhood((x, y), world))
-			if not block and random() < 1/10 and wood in vnns and vnns <= {None, leaves, wood}:
-				world[y][x] = leaves
+			if not block and random() < 1/10:
+				vnns = set(von_neumann_neighborhood((x, y), world))
+				if wood in vnns and vnns <= {None, leaves, wood}:
+					world[y][x] = leaves
 	return world
